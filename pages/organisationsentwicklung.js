f86e778a7aea777c844860e8e7aebeeefb4e67b0
Jens Preussner <jnz+github@posteo.de>
2022-04-17 15:06:21 +0200

    Changed text and icons

--- pages/organisationsentwicklung.js
+++ pages/organisationsentwicklung.js
@@ -58,7 +58,7 @@ export default function Organisationsentwicklung() {
                         Strategieentwicklung und Change
                         </p>
                         <p className="mt-4 max-w-3xl text-xl text-gray-700 lg:mx-auto">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                        Wer sich auf einen Weg macht, hat meistens ein Ziel. Manchmal verändert es sich während der Reise, manchmal geht es schneller, manchmal langsamer. Wenn es notwendig wird, dass Sie ihre Ziele, ihre Fortbewegungsmittel oder ihre Geschwindigkeit ihrer Reise überdenken oder anpassen möchten, unterstütze ich Sie gerne.
                         </p>
                     </div>
                     <div>
@@ -80,7 +80,7 @@ export default function Organisationsentwicklung() {
                                 <dt>
                                 <div className="absolute flex items-center justify-center h-12 w-12 rounded-md text-burgund-600">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fillRule="evenodd" d="M5 2a1 1 0 011 1v1h1a1 1 0 010 2H6v1a1 1 0 01-2 0V6H3a1 1 0 010-2h1V3a1 1 0 011-1zm0 10a1 1 0 011 1v1h1a1 1 0 110 2H6v1a1 1 0 11-2 0v-1H3a1 1 0 110-2h1v-1a1 1 0 011-1zM12 2a1 1 0 01.967.744L14.146 7.2 17.5 9.134a1 1 0 010 1.732l-3.354 1.935-1.18 4.455a1 1 0 01-1.933 0L9.854 12.8 6.5 10.866a1 1 0 010-1.732l3.354-1.935 1.18-4.455A1 1 0 0112 2z" clip-rule="evenodd" />
+                                        <path fillRule="evenodd" d="M5 2a1 1 0 011 1v1h1a1 1 0 010 2H6v1a1 1 0 01-2 0V6H3a1 1 0 010-2h1V3a1 1 0 011-1zm0 10a1 1 0 011 1v1h1a1 1 0 110 2H6v1a1 1 0 11-2 0v-1H3a1 1 0 110-2h1v-1a1 1 0 011-1zM12 2a1 1 0 01.967.744L14.146 7.2 17.5 9.134a1 1 0 010 1.732l-3.354 1.935-1.18 4.455a1 1 0 01-1.933 0L9.854 12.8 6.5 10.866a1 1 0 010-1.732l3.354-1.935 1.18-4.455A1 1 0 0112 2z" clipRule="evenodd" />
                                     </svg>
                                 </div>
                                 <p className="ml-14 text-lg leading-6 font-serif font-medium text-gray-900">Visionen und Werte</p>
